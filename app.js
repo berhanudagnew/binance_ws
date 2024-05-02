419f7a74adac7fce0b827838ca9b2424f4275c2e
adu genet <adugenet@adus-MacBook-Pro.local>
2024-05-02 05:33:47 +0300

    hello 102

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World 101!");
+  res.send("Hello World 102!");
 });
 
 app.listen(port, () => {
