4dc18104155a02c52b1ca358d2b7575a418cd230
adu genet <adugenet@adus-MacBook-Pro.local>
2024-05-02 05:44:32 +0300

    ws first test

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+const WebSocket = require("ws");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const WebSocket = require("ws");
+
+let price = 0;
+
+const ws = new WebSocket("wss://stream.binance.com:9443/ws/atausdt@trade");
+
+ws.on("open", function open() {
+  console.log("Connected.");
+});
+
+ws.on("message", function incoming(data) {
+  const stockObject = JSON.parse(data);
+  price = parseFloat(stockObject.p).toFixed(4);
+  console.log("Price:", price);
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello World 102!");
+  res.send(`Example app listening on port ${price}`);
 });
 
 app.listen(port, () => {
